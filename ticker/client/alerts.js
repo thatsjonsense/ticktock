7ab8c1ed0e9973d1b30d4ec9c5127e6f201c622b
Jon Noronha <cheeselord@gmail.com>
2013-06-27 00:37:21 -0700

    Custom templates for each alert

--- ticker/client/alerts.js
+++ ticker/client/alerts.js
@@ -4,3 +4,13 @@ Template.newsfeed.entries = function () {
     limit: 10
   })
 }
+
+Template.newsfeed_entry.message = function () {
+  /*
+  return Meteor.render(function(){
+    return Template.alert_big_gain(this.data)
+  })*/
+  return Template['alert_' + this.type](this.data)
+
+  
+}
