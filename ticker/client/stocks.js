91b367cff44ec8b7a12996733413194729aecd23
Yuhki Yamashita <yuhki@outlook.com>
2013-06-26 19:29:21 -0700

    CSS around adding stocks

--- ticker/client/stocks.js
+++ ticker/client/stocks.js
@@ -1,23 +1,23 @@
 Session.setDefault('adding_stock',false)
 
 Template.stock_list.stocks = function () {
-  var current_user = Users.findOne(Session.get('user_id'))
-  if (current_user) {
-    return current_user.stocks()  
-  }
+	var current_user = Users.findOne(Session.get('user_id'))
+	if (current_user) {
+		return current_user.stocks()	
+	}
 };
 
 Template.stock_list.title = function () {
-  return Users.findOne(Session.get('user_id')).name + "'s portfolio";
+	return Users.findOne(Session.get('user_id')).name + "'s portfolio";
 }
 
 Template.stock.delta = function () {
-  var ret = (this.price - this.open).toFixed(2);
-  return (ret >= 0 ? "+" : "") + ret;
+	var ret = (this.price - this.open).toFixed(2);
+	return (ret >= 0 ? "+" : "") + ret;
 }
 
 Template.stock.deltaPercent = function () {
-  return (100 * (this.price - this.open) / this.open).toFixed(2) + "%";
+	return (100 * (this.price - this.open) / this.open).toFixed(2) + "%";
 }
 
 Template.stock.updown = function () {
@@ -29,22 +29,24 @@ Template.stock.updown = function () {
 
 
 // Adding a new stock to portfolio
-Template.stock_control.adding_stock = function () {
-  return Session.get('adding_stock')
-}
-
 Template.stock_control.events({
-	'click .add_stock': function (evt) {
-		Session.set('adding_stock',true)
-	},
-
-	'click .submit': function (evt) {
-		Users.update(
-			Session.get('user_id'),
-			{$push: {investments: {
-				symbol: $('.new_stock .symbol').val(),
-				shares: $('.new_stock .shares').val(),
-				cost_basis: $('.new_stock .cost_basis').val()
-			}}})
-	}
+  'click #add_stock': function (evt) {
+    $('#new_stock').show();
+    $('#add_stock').hide();
+  },
+    
+  'click .submit': function (evt) {
+    Users.update(
+      Session.get('user_id'),
+      {$push: {investments: {
+        symbol: $('#new_stock .symbol').val(),
+        shares: $('#new_stock .shares').val(),
+        cost_basis: $('#new_stock .cost_basis').val()
+      }}});
+    // clear values out of control and bring back focus
+    $('#new_stock .symbol').val('').focus();
+    $('#new_stock .shares').val('');
+    $('#new_stock .cost_basis').val('');
+    
+  }
 })
