0ce5cc6f3bc05de20b89e0f15d2567f4d52a5c75
Jon Noronha <cheeselord@gmail.com>
2013-07-27 18:53:56 -0700

    Small refactoring on charts

--- ticker/client/chart.js
+++ ticker/client/chart.js
@@ -1,39 +1,50 @@
 // Let's start with a bar chart of portfolio values
 
 
-// Whenever the chart is rendered...
-Template.chart.rendered = function () {
-	var self = this;
-	self.node = self.find("svg");
+Template.chart_bar.rendered = function () {
+  var self = this;
 
-	// Rerun this function anytime dependencies change
-	if (!self.autorunning) {
-		self.autorunning = Deps.autorun(function() {
-			var users = Users.find({}).fetch();
-      var svg = d3.select(self.node);
-      var bars = svg.selectAll('rect').data(users);
+  if (!self.autorunning) {
+    self.autorunning = Deps.autorun(function() {
+      
+      // Dependencies
+      var users = Users.find({}).fetch();
+      
+
+      // Make the chart
+      var svg = d3.select(self.find('svg'));
+      var bars = svg.select('.bars').selectAll('rect').data(users);
+
+      // Todo: put this into the template. CSS would be even better but how?
+      var HEIGHT = 100;
+      var MARGIN = 1;
 
       var updateBars = function(selection) {
         selection
           .attr("width",function(user) {return (user.currentValue % 100) * 10})
-          .attr("height",10)
-          .attr("y", function(d,i) {return i * 11})
+          .attr("height",HEIGHT)
+          .attr("y", function(d,i) {return i * (HEIGHT + MARGIN)})
+          .attr("class","bar")
       }
-
 
       updateBars(bars.enter().append('rect'));
       updateBars(bars.transition().duration(250).ease('cubic-out'));
       bars.exit().transition().duration(250).attr('width',0).remove();
         
-
-
-		})
-	}
+    });
+  } 
 }
 
 /*
-// Hack to keep chart live updating. Need a better system
-Template.chart.users = function () {
-	return Users.find({}).fetch()
-}
+
+TODO:
+
+Make it less ugly
+Flash red/green depending on up or down since last tick
+Label with names and values
+
+
+Eventually: line graph of one person's portfolio, scrolling. Then everyone's in different colors.
+
+
 */
